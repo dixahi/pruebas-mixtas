948e8d48a81b636f2d2e161a704693a808329f0d
dixahi <59058259+dixahi@users.noreply.github.com>
2019-12-22 21:51:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -86,6 +86,43 @@ window.onload = () => {
     );
 };
 
+$(function(){
+  var flag=0;
+  
+  $('.share').on('click',function(){
+   if(flag == 0)
+    {
+      $(this).siblings('.one').animate({
+      top:'160px',
+      left:'50%',
+    },200);
+    
+     $(this).siblings('.two').delay(200).animate({
+      top:'260px',
+      left:'40%'
+    },200);
+    
+     $(this).siblings('.three').delay(300).animate({
+      top:'260px',
+      left:'60%'
+    },200);
+              
+    $('.one i,.two i, .three i').delay(500).fadeIn(200);  
+      flag = 1;
+    }
+    
+    
+  else{
+    $('.one, .two, .three').animate({
+        top:'300px',
+        left:'50%'
+      },200);
+      
+  $('.one i,.two i, .three i').delay(500).fadeOut(200);
+      flag = 0;
+    }
+  });
+});
 
 document.getElementById("applyEvent").addEventListener("click", function(){
     document.getElementById('hidden-div').style.display = 'block';
